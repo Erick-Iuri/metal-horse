3a723c7013fe3bfd006a2e11bad01a096a6d8c54
Erick <iurierick@gmail.com>
2026-05-13 18:18:46 -0300

    barra de navegação

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,42 @@ export default function Home() {
     /* main div */
     <div>
       {/* Barra de Navegação */}
-      <div className="bg-red-500 ">
-        <nav className="flex ">
-          <div>Logo</div>
-          <div>Barra de pesquisa</div>
-          <div>Usuario/login</div>
-        </nav>
+      <div>
+        <div className="max-w-450 mx-auto flex justify-between items-center p-5">
+          <div>
+            <img src={"/LOGO.png"} width={230} height={100} />
+          </div>
+
+          <div>
+            <input
+              type="text"
+              placeholder="Busque na Metal Horse"
+              className="bg-white text-black 
+              w-300 h-11 p-1
+              px-4 py-2
+              focus:outline-none
+              rounded-sm"
+            />
+          </div>
+
+          <div className="flex gap-4">
+            <div className="flex gap-4">
+              <img src="/icons/Ellipse.png" width={40} height={"auto"} />
+              <div className="pt-2">Olá, Erick</div>
+            </div>
+
+            <div className="flex gap-4">
+              <img src={"/icons/favorite.svg"} 
+              width={30} 
+              height={10} />
+
+              <img src={"/icons/shooping cart.svg"} 
+              width={30} 
+              height={10} />
+            </div>
+          </div>
+          
+        </div>
       </div>
     </div>
   );
