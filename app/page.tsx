fd4d6d68b3ae65e86df1729a1d01873705af28c7
Erick <iurierick@gmail.com>
2026-05-13 11:58:27 -0300

    subindo melhorias basicas

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    /* alguma coisa aqui pra teste */
+    /* main div */
     <div>
-      
+      {/* Barra de Navegação */}
+      <div className="bg-red-500 ">
+        <nav className="flex ">
+          <div>Logo</div>
+          <div>Barra de pesquisa</div>
+          <div>Usuario/login</div>
+        </nav>
+      </div>
     </div>
   );
 }
