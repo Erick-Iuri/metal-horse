dda8584973a3083295b2f6d755874f86c008b545
Erick <iurierick@gmail.com>
2026-05-14 15:17:32 -0300

    Melhoria de banner

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,19 @@ export default function Home() {
     <div>
       {/* Barra de Navegação 🚢 */}
       <div>
-        <div className="flex justify-center space-x-4 max-w-450 mx-auto items-center p-3">
+        <div
+          className="
+        flex 
+        justify-center 
+        space-x-4 
+        max-w-360 
+        mx-auto 
+        items-center 
+        p-3"
+        >
           {/* LOGO da marca 1/3*/}
           <div>
-            <img src={"/LOGO.png"} width={230} height={100} />
+            <Image alt="logo" src={"/LOGO.png"} width={230} height={100} />
           </div>
 
           {/* BARRA DE PESQUISA 2/3*/}
@@ -18,22 +27,43 @@ export default function Home() {
             <input
               type="text"
               placeholder="Busque na Metal Horse"
-              className="bg-white text-black 
-              w-full h-11 p-1
-              px-4 py-2
+              className="
+              bg-white 
+              text-black 
+              w-full 
+              h-11 
+              p-1
+              px-4 
+              py-2
               focus:outline-none
               rounded-sm"
             />
           </div>
 
           {/* USER MENU 3/3 */}
-          <div className="flex space-x-4">
-            <div className="flex space-x-4">
-              <img src="/icons/Ellipse.png" width={40} height={"auto"} />
-              <div className="pt-2">Olá, Erick</div>
+          <div
+            className="
+          flex 
+          space-x-3"
+          >
+            <div className="flex w-30">
+              <Image 
+              src="/icons/Ellipse.png"
+              alt="perfil"
+              width={30}
+              height={30}
+              className="
+              w-auto
+              h-auto
+              object-cover" />
+              <a className="w-20 pt-2">Olá, Erick</a>
             </div>
 
-            <div className="flex space-x-4">
+            <div
+              className="
+            flex 
+            space-x-4"
+            >
               <img src={"/icons/favorite.svg"} width={30} height={10} />
 
               <img src={"/icons/shooping cart.svg"} width={30} height={10} />
@@ -42,11 +72,27 @@ export default function Home() {
         </div>
 
         {/* Botões de ação */}
-        <div className="flex justify-center items-center">
-          <div className="flex space-x-1 p-1 text-lg">
+        <div
+          className="
+        flex 
+        justify-center 
+        items-center"
+        >
+          <div
+            className="
+          flex 
+          space-x-1 
+          p-1 
+          text-lg"
+          >
             <Link
               href="/"
-              className="px-8 py-1.5 bg-[#E82A25] rounded-sm font-semibold"
+              className="
+              px-8 
+              py-1.5 
+              bg-[#E82A25] 
+              rounded-sm 
+              font-semibold"
             >
               Conexões
             </Link>
@@ -55,9 +101,13 @@ export default function Home() {
               className="
               hover:bg-white 
               hover:text-black
-                duration-300
-                ease-in-out
-              transition py-1 px-5 border-2 rounded-sm"
+              duration-300
+              ease-in-out
+              transition 
+              py-1 
+              px-5 
+              border-2 
+              rounded-sm"
             >
               Mangueiras
             </Link>
@@ -148,14 +198,33 @@ export default function Home() {
         </div>
 
         {/* Faixa vermelha */}
-        <div className="bg-[#E82A25] w-full h-2 my-2"></div>
+        <div
+          className="
+        bg-[#E82A25] 
+        w-full 
+        h-2 
+        my-2"
+        ></div>
       </div>
 
       {/* Banners */}
-      <div className="flex justify-center overflow-hidden">
-        <div className="flex w-430 h-90 bg-amber-400 relative ">
-          <div className="flex w-430 h-90 bg-blue-400 absolute left-432"></div>
-        </div>
+      <div
+        className=" 
+        w-full
+        max-w-360
+        mx-auto h-120 bg-red-500"
+      >
+        <Image
+          src="/banners/banner.jpg"
+          alt="banner"
+          width={1300}
+          height={400}
+          className="
+          w-full
+          h-full
+          object-cover"
+        />
+        {/* flutuante */}
       </div>
     </div>
   );
